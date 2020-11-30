4e5d6042d2939198b92eee45cf7729e334ea293d
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2020-11-30 14:32:22 +0900

    first commit

--- src/Frame.java
+++ src/Frame.java
@@ -1,9 +1,20 @@
+import javax.swing.JFrame;
 
-public class Frame {
+public class Frame extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+	
+	public Frame() {
+		this.setTitle("○×ゲーム");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		
+		this.pack();
+		this.setVisible(true);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Frame();
 	}
 
 }

965bf41034187b9fb88b643a507b8bf92525b768
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2020-12-02 11:29:41 +0900

    first commit

--- src/Frame.java
+++ src/Frame.java
@@ -1,6 +1,12 @@
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Frame {
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class Frame implements ActionListener {
 	
 	private static JFrame frame;
 	
@@ -13,6 +19,32 @@ public class Frame {
 		frame.setTitle("○×ゲーム");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
+		
+		JMenuBar jmb = new JMenuBar();
+		frame.setJMenuBar(jmb);
+		
+		JMenu menu = new JMenu("メニュー");
+		jmb.add(menu);
+		
+		JMenuItem mainmenu = new JMenuItem("メインメニュー");
+		mainmenu.addActionListener(this);
+		mainmenu.setActionCommand("mainmenu");
+		menu.add(mainmenu);
+		
+		JMenuItem start = new JMenuItem("ゲーム開始");
+		start.addActionListener(this);
+		start.setActionCommand("start");
+		menu.add(start);
+		
+		JMenuItem setting = new JMenuItem("設定画面");
+		setting.addActionListener(this);
+		setting.setActionCommand("setting");
+		menu.add(setting);
+		
+		JMenuItem quit = new JMenuItem("ゲーム終了");
+		quit.addActionListener(this);
+		quit.setActionCommand("quit");
+		menu.add(quit);
 		
 		menuPanel = new Menu();
 		settingPanel = new Setting();
@@ -46,6 +78,22 @@ public class Frame {
 
 	public static void main(String[] args) {
 		new Frame();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand().equals("mainmenu")) {
+			changePanel("menuPanel");
+		}
+		else if(e.getActionCommand().equals("start")) {
+			changePanel("gamePanel");
+		}
+		else if(e.getActionCommand().equals("setting")) {
+			changePanel("settingPanel");
+		}
+		else if(e.getActionCommand().equals("quit")) {
+			System.exit(1);
+		}
 	}
 
 }
